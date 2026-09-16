b0d5959d4f8546ce551bc38c9bb8365412c2eba3
adedayobabalola18-rgb <adedayobabalola18@gmail.com>
2026-09-16 16:51:29 +0100

    fix: adjust button component structure for improved readability

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -9,8 +9,7 @@ export function Button({ title = "Button", color, borderRadius}) {
                 confirm(`${title} was clicked!`);
             }}
             className={`mx-auto block my-5 py-2 px-5 ${color} ${borderRadius}`}
-            >
-
+            >{title}
         </button>
     );
 }
